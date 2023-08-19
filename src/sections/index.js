b698d451de7b22549a9b0d4be2386f0e691c05c6
Shoaib-A <shoaibashraf3733@gmail.com>
2023-08-19 17:24:05 +0500

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import SuperQuality from './SuperQuality';
+import Footer from './Footer';
+import Subscribe from './Subscribe';
+import SpecialOffer from './SpecialOffer';
+import Reviews from './Reviews';
+import Services from './Services';
+
+export default {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Footer,
+    Subscribe,
+    SpecialOffer,
+    Reviews,
+    Services
+}
